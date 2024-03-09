b10e88aac95209d036fb0c1227a1f01e36038259
SUJOY GIRI <sujoy.giri@infosys.com>
2024-03-09 17:23:19 +0530

    added deal detail component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DealDetailsComponent } from './deal-details/deal-details.component';
 
 export const routes: Routes = [
     {path:"", redirectTo:"/home", pathMatch:"full"},
     {path:"home", component: HomeComponent},
     {path:"vacations", loadComponent: () => import('./vacation/vacation.component').then(module => module.VacationComponent)},
+    {path:"deals",component:DealDetailsComponent},
     {path:"destination",loadChildren:() => import('./destination/destination.routes').then(module => module.DESTINATION_ROUTES)},
     {path: "**", redirectTo: "/home"}
 ];
---
+++ src/app/deal-details/deal-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deal-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './deal-details.component.html',
+  styleUrl: './deal-details.component.css'
+})
+export class DealDetailsComponent {
+
+}
