1a373434fd511c1eac78279bf5d88f5a5ae4d6cd
Sujoy Giri <girisujoy630@gmail.com>
2024-03-12 07:02:36 +0530

    fixed title bar

--- src/app/titlebar/titlebar.component.ts
+++ src/app/titlebar/titlebar.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-titlebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './titlebar.component.html',
   styleUrl: './titlebar.component.css'
 })
 export class TitlebarComponent implements OnInit {
   ngOnInit(): void {
-      
+
   }
 }
